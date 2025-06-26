c326a0eb13096853652947f67a374d1646718f8a
scwong85 <sweechong.wong@gmail.com>
2025-06-26 22:17:28 +0300

    use smart cache

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,10 @@ from retriever import retrieve_context
 from cache import Cache
 import uvicorn
 import openai
+from openai import OpenAI
 import anthropic
 import time
 import random
-import openai
 from config import Config
 from langchain_anthropic import ChatAnthropic
 from langchain.prompts import PromptTemplate
@@ -19,20 +19,99 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 import redis
 import json
+import numpy as np
+from redis.commands.search.field import TextField, VectorField
+from redis.commands.search.index_definition import IndexDefinition, IndexType
+from redis.commands.search.query import Query
+
 
 USE_CLAUDE = os.getenv("USE_CLAUDE", "false").lower() == "true"
+# === CONFIGURATION ===
+EMBEDDING_DIM = 1536
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+VECTOR_INDEX = "semantic_cache"
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 cache = Cache()
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
 )
-
+openai_client = OpenAI()
 redis_client = redis.from_url(Config.REDIS_URL)
 
 
-class Query(BaseModel):
+class QuestionQuery(BaseModel):
     question: str
+
+
+def embed(text):
+    res = openai_client.embeddings.create(
+        model="text-embedding-3-small",
+        input=[text]
+    )
+    return np.array(res.data[0].embedding, dtype=np.float32)
+
+
+# === REDIS VECTOR INDEX SETUP ===
+def create_index():
+    try:
+        redis_client.ft(VECTOR_INDEX).info()
+    except:
+        print("Index not found, creating...")
+        redis_client.ft(VECTOR_INDEX).create_index(
+            fields=[
+                TextField("question"),
+                TextField("answer"),
+                VectorField("embedding", "FLAT", {
+                    "TYPE": "FLOAT32",
+                    "DIM": EMBEDDING_DIM,
+                    "DISTANCE_METRIC": "COSINE",
+                })
+            ],
+            definition=IndexDefinition(prefix=["q:"], index_type=IndexType.HASH)
+        )
+
+
+# === SEARCH CACHE ===
+def search_similar_question(question, threshold=0.2):
+    print("searching for answer for question", question)
+    vector = embed(question).tobytes()
+    q = Query("*=>[KNN 1 @embedding $vec_param AS score]") \
+        .sort_by("score") \
+        .return_fields("question", "answer", "score") \
+        .dialect(2)
+    params = {"vec_param": vector}
+
+    res = redis_client.ft(VECTOR_INDEX).search(q, query_params=params)
+    print("response from cache")
+    print(res)
+    if res.total > 0 and float(res.docs[0].score) < threshold:
+        print("cached matched")
+        return json.loads(res.docs[0].answer)
+    return None
+
+
+# === SAVE TO CACHE ===
+def save_to_cache(question: str, answer: dict):
+    vector = embed(question).tobytes()
+    # redis_client.hset(
+    #     f"q:{question}",
+    #     mapping={
+    #         "question": question,
+    #         "answer": json.dumps(answer),
+    #         "embedding": vector
+    #     }
+    # )
+    redis_client.ft(VECTOR_INDEX).add_document(
+        f"q:{question}",
+        replace=True,
+        question=question,
+        answer=json.dumps(answer),
+        embedding=vector
+    )
 
 
 def call_model(context, question):
@@ -165,21 +244,37 @@ async def rate_limiter(request: Request, call_next):
 
 
 @app.post("/ask")
-async def ask(q: Query):
-    cached = cache.get(q.question)
+async def ask(q: QuestionQuery):
+    # cached = cache.get(q.question)
+    # if cached:
+    #     print("Getting answer from cache")
+    #     cache.refresh_async(
+    #         q.question, lambda text: call_model(retrieve_context(text), text)
+    #     )
+    #     # return {"answer": cached.decode()}
+    #     return json.loads(cached.decode())
+
+    # print("Getting answer from LLM")
+    # context = retrieve_context(q.question)
+    # # answer = call_model(context, q.question)
+    # response = call_rag(context, q.question)
+    # cache.set(q.question, json.dumps(response))
+    # return response
+    # semantic cache
+    create_index()
+    cached = search_similar_question(q.question)
     if cached:
-        print("Getting answer from cache")
-        cache.refresh_async(
-            q.question, lambda text: call_model(retrieve_context(text), text)
-        )
-        # return {"answer": cached.decode()}
-        return json.loads(cached.decode())
-    print("Getting answer from LLM")
+        return cached
+
+    print("Cache miss → calling RAG")
     context = retrieve_context(q.question)
-    # answer = call_model(context, q.question)
+    # print("context is", context)
     response = call_rag(context, q.question)
-    cache.set(q.question, json.dumps(response))
+    save_to_cache(q.question, response)
     return response
+
+
+
 
 
 if __name__ == "__main__":
